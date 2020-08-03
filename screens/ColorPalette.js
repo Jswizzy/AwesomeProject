16fff131d4ced0d4cd74998de931b9294a48685b
Justin Smith <smithj@g.cofc.edu>
2020-08-03 13:49:12 -0400

    feat: add new palettes

--- screens/ColorPalette.js
+++ screens/ColorPalette.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, RefreshControl } from 'react-native';
 import { ColorBox } from '../components/ColorBox';
 
 export const ColorPalette = ({ route }) => {
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { FlatList, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { PalettePreview } from '../components/PalettePreview';
 
 const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
 
-export const Home = ({ navigation }) => {
+export const Home = ({ navigation, route }) => {
+  const newPalette = route.params ? route.params.newPalette : undefined;
   const [palettes, setPalettes] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   const handleFetchPalettes = useCallback(async () => {
     const response = await fetch(URL);
     if (response.ok) {
-      const palettes = await response.json();
-      setPalettes(palettes);
+      const newPalettes = await response.json();
+      setPalettes(newPalettes);
     }
   }, []);
 
@@ -19,9 +21,26 @@ export const Home = ({ navigation }) => {
     handleFetchPalettes();
   }, [handleFetchPalettes]);
 
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    await handleFetchPalettes();
+    const handle = setTimeout(() => {
+      setIsRefreshing(false);
+    }, 1000);
+    return () => clearTimeout(handle);
+  }, [handleFetchPalettes]);
+
+  useEffect(() => {
+    if (newPalette) {
+      setPalettes((colors) => [newPalette, ...colors]);
+    }
+  }, [newPalette]);
+
   return (
     <FlatList
       style={styles.list}
+      onRefresh={handleRefresh}
+      refreshing={isRefreshing}
       data={palettes}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
@@ -32,12 +51,32 @@ export const Home = ({ navigation }) => {
           colorPalette={item}
         />
       )}
+      ListHeaderComponent={
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('AddNewPalette')}
+        >
+          <Text style={styles.buttonText}>Add New Palette</Text>
+        </TouchableOpacity>
+      }
     />
   );
 };
 
 const styles = StyleSheet.create({
   list: {
-    padding: 8,
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  button: {
+    height: 50,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'teal',
   },
 });
